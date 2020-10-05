3da6a3df5e4946328c879c37ce9692da26a87a37
narren <clevercodernarren1029@gmail.com>
2020-10-05 13:31:57 +0530

    we are going to deploy a react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <h1>About my self</h1>
+        <div className="app__bio">
+          <h2>My name is narren</h2>
+          <h2>I want to become a great coder !!!</h2>
+          <h2>Mother : Manjula</h2>
+          <h2>Father : Surendra</h2>
+          <h2> age : 13 </h2> 
+        </div>
+      </div>
     </div>
   );
 }
