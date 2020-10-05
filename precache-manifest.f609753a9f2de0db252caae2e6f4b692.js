dba876f86f183068a31fb4ceda675779c7893d76
narren <clevercodernarren1029@gmail.com>
2020-10-05 13:30:13 +0530

    Updates

---
+++ precache-manifest.f609753a9f2de0db252caae2e6f4b692.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77daa07b8251d3103761e875892a628e",
+    "url": "/react-app-deploy/index.html"
+  },
+  {
+    "revision": "bc20c531d71a024f2aa1",
+    "url": "/react-app-deploy/static/css/main.4c3c1645.chunk.css"
+  },
+  {
+    "revision": "c2c8327961b9b8f6da7d",
+    "url": "/react-app-deploy/static/js/2.d9ab88f3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-deploy/static/js/2.d9ab88f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc20c531d71a024f2aa1",
+    "url": "/react-app-deploy/static/js/main.a359f0c8.chunk.js"
+  },
+  {
+    "revision": "d01b758bfb22da8159bf",
+    "url": "/react-app-deploy/static/js/runtime-main.1a6599d0.js"
+  }
+]);
